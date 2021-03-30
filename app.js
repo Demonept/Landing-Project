17fd72e941935fdb043928915d24caec313579cc
Demonept <66496493+Demonept@users.noreply.github.com>
2021-03-30 16:49:48 +0300

    Js штуки
    И еще много всякой не самой нужной фигни

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 $(function() {
+	
+	
+	/*Fixed Header*/
 	let header = $("#header");
 	let intro = $("#intro");
 	let introH = intro.innerHeight();
 	let scrollPos = $(window).scrollTop();
 	
 	
-	$(window).on("scroll",function(){
+	$(window).on("scroll load resize",function(){
+		introH = intro.innerHeight();
 		scrollPos = $(this).scrollTop();
-		console.log(scrollPos);
 		
 		if (scrollPos > introH) {
 			header.addClass("fixed");
@@ -15,14 +18,50 @@ $(function() {
 		}else{
 			header.removeClass("fixed");
 			
-			//code
 		}
 	});
 	
 	
-	console.log(scrollPos);
+	/* Smooth scroll */
 	
-	header.addClass("fixed");
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+		
+		
+		let elementId = $(this).data('scroll');
+		let elementOffset = $(elementId).offset().top;
+		
+		nav.removeClass("show");
+		
+		$("html,body").animate({
+			scrollTop: elementOffset -45
+		},700);
+		
+		});
 	
 	
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+	
+	navToggle.on("click", function(event){
+		event.preventDefault();
+		nav.toggleClass("show");
+		
+		});
+	
+	
+	
+	let slider = $("#reviewsSlider");
+	
+	slider.slick({
+		infinite: true,
+		sliderToShow: 1,
+		sliderToScroll: 1,
+		fade:false,
+		arrows:false,
+		dots:true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		
+	});
 });
